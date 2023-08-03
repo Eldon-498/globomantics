dcf6e83fccf33ee685eed3841c42f6c0bc166405
Eldon-498 <eldondaniel2@gmail.com>
2023-08-03 09:50:14 +0300

    featured House component

---
+++ src/main-page/featured-house.js
@@ -0,0 +1,18 @@
+const featuredHouse = ({ house }) => {
+    if(house)
+    return (  
+        <div>
+            <div className="row featuredHouse">
+                <h3 className="col-mid-12 text-center">Featured House</h3>
+            </div>
+            <House house={house} />
+        </div>
+
+
+    );
+    return(
+        <div>No featured house at this time</div>
+    )
+}
+ 
+export default featuredHouse;
