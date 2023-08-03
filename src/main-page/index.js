27f83c105d6fc0cab5f176a95d9fbd2637d014de
Eldon-498 <eldondaniel2@gmail.com>
2023-08-03 09:43:31 +0300

    Routing components

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,11 +1,43 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { useEffect, useState, useMemo } from 'react';
 import './main-page.css';
 import Header from './header';
 
 function App() {
+
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(()=>{
+    const fetchHouses = async () =>{
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    }
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if(allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }    
+
+  }, [allHouses]);
+
+  
   return (
-    <div className='container'>
-      <Header title="Providing houses all over the world"/>
+    <Router>
+    <div className="'container'">
+      <Header subtitle="Providing houses all over the world"/>
     </div>
+    <Switch>
+      <Route path="/">
+
+      </Route>
+    </Switch>
+
+    </Router>
   );
 }
 
