5b7ccf42fdd3d48d2467641561715c7ea066bd3e
Eldon-498 <eldondaniel2@gmail.com>
2023-08-04 13:49:26 +0300

    search results

--- src/main-page/house-filter.js
+++ src/main-page/house-filter.js
@@ -1,14 +1,23 @@
+import { useNavigation }from 'react-router-dom'
+
 const HouseFilter = ( {allHouses}) => {
     const countires = allHouses
     ? Array.from(new Set(allHouses.map((h)=> h.country))) : [];
     countires.unshift(null);
+    const history = useNavigation();
+
+    const onSearchChange = (e) => {
+        const country = e.target.value;
+        history.push(`/searchresults/${country}`)
+    }
+
     return ( 
         <div className="row mt-3">
         <div className="offset-mid-2 col-md-4">
             Looking for your dream house in country:
         </div>
         <div className="col-md-4 mb-3">
-            <select className="form-select">
+            <select className="form-select" onChange={{onSearchChange}}>
                 {countires.map((c)=>(
                     <opton key={c} value={c}>
                         {c}
